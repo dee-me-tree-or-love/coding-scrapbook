7acf2008698fc3871cd5a32b6ad5a76bc3df803c
Dmitrii Orlov <dmitrii@promaton.com>
2021-12-20 14:52:08 +0100

    Simple specialized Quotes Router

--- Elixir/random-quote/lib/random_quote/router.ex
+++ Elixir/random-quote/lib/random_quote/router.ex
@@ -1,11 +1,32 @@
+defmodule RandomQuote.QuoteRouter do
+  use Plug.Router
+  require Logger
+
+  plug(Plug.Logger, log: :debug)
+  plug(:match)
+  plug(:dispatch)
+
+  # --------------------
+  # Routes go below here
+  # --------------------
+
+  # Simple GET Request handler for path /hello
+  get "/random" do
+    send_resp(conn, 200, "yo.")
+  end
+end
+
 defmodule RandomQuote.Router do
   use Plug.Router
   use Plug.Debugger
   require Logger
+
+  # NB: aliased modules in the same folder should be defined above
+  alias RandomQuote.QuoteRouter, as: QuoteRouter
+
   plug(Plug.Logger, log: :debug)
 
   plug(:match)
-
   plug(:dispatch)
 
   # --------------------
@@ -16,4 +37,7 @@ defmodule RandomQuote.Router do
   get "/health" do
     send_resp(conn, 200, "It's ok.")
   end
+
+  # Forward routes to QuoteRouter
+  forward("/quotes", to: QuoteRouter)
 end
