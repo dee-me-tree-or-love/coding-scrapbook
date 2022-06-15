650a5c0bef006aecd3a23c9e8f47be021be7dc70
Dmitrii Orlov <dmtree.dev@yahoo.com>
2022-06-15 18:11:27 +0200

    basic setup of timeboxy haskell project

---
+++ Haskell/timeboxy/ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for timeboxy
+
+## Unreleased changes
---
+++ Haskell/timeboxy/app/Main.hs
@@ -0,0 +1,11 @@
+module Main where
+
+import Lib (someFunc)
+
+-- TODO:
+-- [ ] implement CLI interface to add work items
+-- [ ] implement local tiny DB (CSV,JSON, or else?) for persistence
+-- [ ] implement basic aggregation output with extensible visualizers
+
+main :: IO ()
+main = someFunc
